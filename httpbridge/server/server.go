896527d5663340ddaf7c3d3d15dc9815e3a55d09
hasuburero <hasuburero0802@gmail.com>
2024-10-15 21:31:00 +0900

    2024.10.15.3 edit httpbridge

--- httpbridge/server/server.go
+++ httpbridge/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -10,7 +11,26 @@ const (
 	port = "8080"
 )
 
+type Get_echonet_request struct {
+	gw_id  string `json:"gw_id"`
+	format string `json:"format"`
+}
+
+type Get_echonet_response struct {
+	format string
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	length := r.ContentLength
+	reqBody := make([]byte, length)
+	var ctx Get_echonet_request
+	err := json.Unmarshal(reqBody, &ctx)
+	if err != nil {
+		return
+	}
+	fmt.Println(ctx)
+
+	var format Get_echonet_response{format:"1234657890"}
 }
 
 func main() {
